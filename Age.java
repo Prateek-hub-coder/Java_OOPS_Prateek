a13dd73adfa7a92f6364513cd77b9142fdffeef7
Prateek-hub-coder <prateekdwivedi012@gmail.com>
2025-08-06 16:36:05 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Age {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age ");
+        int age = sc.nextInt();
+        System.out.println("My age is"+ age);
+    }
+    
+}
